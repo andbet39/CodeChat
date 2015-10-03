fe05f88839704567c3813c0f130ecb4c32abce7f
Andrea Terzani <andrea.terzani@gmail.com>
2015-10-03 17:10:17 +0200

    review

--- common/models/chat-user.js
+++ common/models/chat-user.js
@@ -1,3 +1,22 @@
 module.exports = function(ChatUser) {
-
+	
+	ChatUser.joinroom = function(room_id,user_id, cb) {
+		console.log("joinroom remote");
+	    ChatUser.app.models.Room.findById(room_id,function(err,room){
+	    		ChatUser.findById(user_id,function(err,data){
+					data.joinedrooms.add(room,function(err,res){
+					cb();
+				});
+			});
+	    });
+	   };
+ 
+	  ChatUser.remoteMethod('joinroom', {
+	    accepts: [
+	      {arg: 'room_id', type: 'string'},
+	      {arg: 'user_id', type: 'string'}
+	     ],
+	    returns: {arg: 'success', type: 'boolean'},
+	    http: {path:'/joinroom', verb: 'post'}
+	  });
 };
